ce08f0117661baa6114b90506370101d9d2592c2
Tayyab Khan <tkhan@atlassian.com>
2022-05-21 13:00:31 +0530

    updated port from 80 to 443

--- main.tf
+++ main.tf
@@ -16,8 +16,8 @@ resource "aws_security_group" "sg" {
   vpc_id      = "vpc-bbd4bdc0"
 
   ingress {
-    from_port        = 80
-    to_port          = 80
+    from_port        = 443
+    to_port          = 443
     protocol         = "tcp"
     cidr_blocks      = [ "10.0.0.0/24" ]
   }
